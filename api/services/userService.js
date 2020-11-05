3d2735949878aa2bf11ba34878ac0f80117d10f6
Dmytro Orlov <dorlov@amplify.com>
2020-11-05 20:52:32 +0200

    remove unnecessary try/catch blocks

--- api/services/userService.js
+++ api/services/userService.js
@@ -2,66 +2,44 @@ import database from "../../db/models";
 
 class userService {
   static async getAllUsers() {
-    try {
-      return await database.user.findAll();
-    } catch (error) {
-      throw error;
-    }
+    return await database.user.findAll();
   }
 
   static async addUser(newUser) {
-    try {
-      return await database.user.create(newUser);
-    } catch (error) {
-      throw error;
-    }
+    return await database.user.create(newUser);
   }
 
   static async updateUser(id, updatedUser) {
-    try {
-      const userToUpdate = await database.user.findOne({
-        where: { id: Number(id) },
-      });
+    const userToUpdate = await database.user.findOne({
+      where: { id: Number(id) },
+    });
 
-      if (userToUpdate) {
-        await database.user.update(updatedUser, { where: { id: Number(id) } });
-
-        return updatedUser;
-      }
-      return null;
-    } catch (error) {
-      throw error;
+    if (userToUpdate) {
+      await database.user.update(updatedUser, { where: { id: Number(id) } });
+      return updatedUser;
     }
+    return null;
   }
 
   static async getUser(id) {
-    try {
-      const user = await database.user.findOne({
-        where: { id: Number(id) },
-      });
-
-      return user;
-    } catch (error) {
-      throw error;
-    }
+    const user = await database.user.findOne({
+      where: { id: Number(id) },
+    });
+    return user;
   }
 
   static async deleteUser(id) {
-    try {
-      const userToDelete = await database.user.findOne({
+    const userToDelete = await database.user.findOne({
+      where: { id: Number(id) },
+    });
+
+    if (userToDelete) {
+      const deletedUser = await database.user.destroy({
         where: { id: Number(id) },
       });
-
-      if (userToDelete) {
-        const deletedUser = await database.user.destroy({
-          where: { id: Number(id) },
-        });
-        return deletedUser;
-      }
-      return null;
-    } catch (error) {
-      throw error;
+      return deletedUser;
     }
+    return null;
   }
 }
 
